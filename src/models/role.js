549c737e958531fd9752f281ec4d50c2de025b7c
masb0ymas <fajriajjha@gmail.com>
2020-07-30 21:04:05 +0700

    fix: model role

--- src/models/role.js
+++ src/models/role.js
@@ -1,15 +1,10 @@
+import SequelizeAttributes from 'utils/SequelizeAttributes'
+
 module.exports = (sequelize, DataTypes) => {
   const Role = sequelize.define(
     'Role',
     {
-      id: {
-        type: DataTypes.UUID,
-        primaryKey: true,
-        defaultValue: DataTypes.UUIDV4,
-        allowNull: false,
-        autoIncrement: false,
-      },
-      nama: DataTypes.STRING,
+      ...SequelizeAttributes.Roles,
     },
     {}
   )
