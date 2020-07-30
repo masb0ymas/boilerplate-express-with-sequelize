f19b2b2501493ca87bd56748cd202833c13f51fb
masb0ymas <fajriajjha@gmail.com>
2020-07-30 21:21:47 +0700

    add: log query parameter

--- src/models/index.js
+++ src/models/index.js
@@ -13,12 +13,10 @@ let sequelize
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config)
 } else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  )
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    ...config,
+    logQueryParameters: true,
+  })
 }
 
 fs.readdirSync(__dirname)
